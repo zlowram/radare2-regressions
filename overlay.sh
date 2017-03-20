91df4780b2d9d8a7b5c5a0325f74be1f460a42a3
pancake <pancake@nopcode.org>
2017-03-20 01:27:03 +0100

    More goodies for overlay

--- overlay.sh
+++ overlay.sh
@@ -9,7 +9,7 @@ if [ ! -d ../t ]; then
 fi
 
 O=../t/overlay
-B=`cd $O ; git rev-parse --abbrev-ref HEAD`
+B=`cd $O 2>/dev/null && git rev-parse --abbrev-ref HEAD`
 
 create() {
 	if [ "$B" = master ];then
@@ -21,7 +21,7 @@ create() {
 	if [ -d "$O" ]; then
 	(
 		cd ../t
-		git rm -r -f overlay/$B
+		git rm -rf "overlay/$B"
 	)
 	fi
 	rm -rf $O
@@ -61,14 +61,14 @@ apply() {
 		exit 1
 	fi
 	# git reset --hard
-	OL=$(cd $O/overlay 2> /dev/null && ls)
+	OL=$(cd "$O" 2> /dev/null && ls)
 	if [ -z "$OL" ]; then
 		echo "Cannot find any overlay to apply"
 		exit 0
 	fi
 	for o in $OL ; do
 	(
-		F=`cd "$O/$o" ; find . -type f`
+		F=`cd "$O/$o" 2> /dev/null && find . -type f`
 		for a in $F ; do
 			if [ -n "`echo $a | grep .patch`" ]; then
 				patch -p1 < "$O/$o/$a"
@@ -80,11 +80,12 @@ apply() {
 			fi
 		done
 	)
+	done
 	echo "Applied radare2/t/overlay into r2r. You may want to commit that in r2r."
 }
 
 case "$1" in
-|-h)
+''|-h)
 	echo "Usage: ./overlay.sh [auto|apply|create]"
 	echo " auto   - if r2.branch == master { apply }{ create }"
 	echo " create - creates ../t/overlay with changes in this r2r"
