87df72f3fa10ce4a92d751fd44799f353cd7eb74
kriw <kotarou777775@gmail.com>
2017-12-08 04:23:53 +0100

    Add tests for unit/test_unum.c (#1086)
    * add tests for unit/test_unum.c
    
    * fix typo

--- unit/test_unum.c
+++ unit/test_unum.c
@@ -40,17 +40,71 @@ bool test_r_num_between() {
 	mu_assert_eq (r_num_between (num, "3 2 1"), 0, "3 <= 2 <= 1");
 	mu_assert_eq (r_num_between (num, "1 1 1"), 1, "1 <= 1 <= 1");
 	mu_assert_eq (r_num_between (num, "2 1 3"), 0, "2 <= 1 <= 3");
-	mu_assert_eq (r_num_between (num, "1 2 (1+2)"), 1, "1 <= 2 <= (1+2)");
-	mu_assert_eq (r_num_between (num, "2 3 (1+2+3)"), 1, "2 <= 3 <= (1+2+3)");
-	mu_assert_eq (r_num_between (num, "(1+2) 2 (1+1)"), 0, "(1+2) <= 2 <= (1+1)");
+	mu_assert_eq (r_num_between (num, "1 2 1+2"), 1, "1 <= 2 <= 1+2");
+	mu_assert_eq (r_num_between (num, "2 3 1+2+3"), 1, "2 <= 3 <= 1+2+3");
+	mu_assert_eq (r_num_between (num, "1+2 2 1+1"), 0, "1+2 <= 2 <= 1+1");
+	mu_assert_eq (r_num_between (num, "1 + 2 2 1 + 1"), 0, "1 + 2 <= 2 <= 1 + 1");
 	mu_end;
 }
 
+bool test_r_num_str_len() {
+	mu_assert_eq (r_num_str_len ("1"), 1, "\"1\"");
+	mu_assert_eq (r_num_str_len ("1+1"), 3, "\"1+1\"");
+	mu_assert_eq (r_num_str_len ("1 + 1"), 5, "\"1 + 1\"");
+	mu_assert_eq (r_num_str_len ("1 + 1 "), 5, "\"1 + 1 \"");
+	mu_assert_eq (r_num_str_len ("1 + 1  "), 5, "\"1 + 1  \"");
+	mu_assert_eq (r_num_str_len ("1 + 1 1"), 5, "\"1 + 1 1\"");
+	mu_assert_eq (r_num_str_len ("1 + 1 1 + 1"), 5, "\"1 + 1 1 + 1\"");
+	mu_assert_eq (r_num_str_len ("1 + (1 + 1) 1"), 11, "\"1 + (1 + 1) 1\"");
+	mu_assert_eq (r_num_str_len ("1 + (1 + (1 + 1)) 1"), 17, "\"1 + (1 + (1 + 1)) 1\"");
+	mu_assert_eq (r_num_str_len ("1+(1+(1+1)) 1"), 11, "\"1+(1+(1+1)) 1\"");
+	mu_assert_eq (r_num_str_len ("(1 + 1) + (1 + 1) 1"), 17, "\"(1 + 1) + (1 + 1) 1\"");
+	mu_assert_eq (r_num_str_len ("(1+1)+(1+1) 1"), 11, "\"(1+1)+(1+1) 1\"");
+    mu_end;
+}
+
+bool test_r_num_str_split() {
+	char *str = malloc (0x20);
+	strcpy (str, "1 1 + 2 1 + (2 + 3) 4 ");
+	//expected "1\01 + 2\01 + (2 + 3)\04\0"
+	int i, count = r_num_str_split (str);
+	mu_assert_eq (count, 4, "r_num_str_split (str) == 4");
+	mu_assert_streq (str+0, "1", "1");
+	mu_assert_streq (str+2, "1 + 2", "1 + 2");
+	mu_assert_streq (str+8, "1 + (2 + 3)", "1 + (2 + 3)");
+	mu_assert_streq (str+20, "4", "4");
+	free (str);
+    mu_end;
+}
+
+bool test_r_num_str_split_list() {
+	char *s;
+	char *str = malloc (0x20);
+	strcpy (str, "1 1 + 2 1 + (2 + 3) 4 ");
+	//expected {"1", "1 + 2", "1 + (2 + 3)", "4"} as list
+	RList *list = r_num_str_split_list (str);
+	mu_assert_eq (r_list_length (list), 4, "r_list_length (list) == 4");
+	s = (char *)r_list_pop_head (list);
+	mu_assert_streq (s, "1", "1");
+	s = (char *)r_list_pop_head (list);
+	mu_assert_streq (s, "1 + 2", "1 + 2");
+	s = (char *)r_list_pop_head (list);
+	mu_assert_streq (s, "1 + (2 + 3)", "1 + (2 + 3)");
+	s = (char *)r_list_pop_head (list);
+	mu_assert_streq (s, "4", "4");
+	free (str);
+	r_list_free (list);
+    mu_end;
+}
+
 bool all_tests() {
-	test_r_num_units ();
-	test_r_num_minmax_swap_i ();
-	test_r_num_minmax_swap ();
-	test_r_num_between ();
+	mu_run_test (test_r_num_units);
+	mu_run_test (test_r_num_minmax_swap_i);
+	mu_run_test (test_r_num_minmax_swap);
+	mu_run_test (test_r_num_between);
+	mu_run_test (test_r_num_str_len);
+	mu_run_test (test_r_num_str_split);
+	mu_run_test (test_r_num_str_split_list);
 	return tests_passed != tests_run;
 }
 
