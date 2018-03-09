b571e9e554e0fdcd74e8c44981c0839d13886207
zlowram <zlowram@gmail.com>
2018-03-09 01:27:36 +0100

    Fixed fuzzing target + makefile (#1226)

--- fuzz/targets/ia_fuzz.cc
+++ fuzz/targets/ia_fuzz.cc
@@ -6,19 +6,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 	char path[128];
 
 	r = r_core_new();
-	r_config_set(r->config, "scr.interactive", "false");
-	r_config_set(r->config, "scr.prompt", "false");
-	r_config_set(r->config, "scr.color", "false");
 
-	snprintf(path, sizeof(path)-1, "malloc://%d", (int)Size);
-	RCoreFile *fh = r_core_file_open (r, path, R_IO_WRITE | R_IO_READ, 0LL);
-	r_core_bin_load(r, NULL, UT64_MAX);
+	r_core_cmdf(r, "o malloc://%d", Size);
 	r_io_write_at(r->io, 0, Data, Size);
 
-	r_core_cmd0(r, "oba");
+	r_core_cmd0(r, "oba 0");
 	r_core_cmd0(r, "ia");
 
-	r_core_file_free (fh);
 	r_core_free(r);
 	return 0;
 }
